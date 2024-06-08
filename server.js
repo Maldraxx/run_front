0370bff4b79403b20c43b097b913b8e3a472e2b0
BHW <kassadin34@gmail.com>
2024-06-09 02:40:54 +0900

    server js 수정

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname + '/dist')).listen(8080, function(){
-    console.log('Server running on 8080...');
-});
+var port = process.env.PORT || 8080;
 
-// npm run build
+connect().use(serveStatic(__dirname + '/dist')).listen(port, function(){
+    console.log('Server running on ' + port + '...');
+});
