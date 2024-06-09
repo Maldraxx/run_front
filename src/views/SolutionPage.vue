1e0c37c38403294a79ff8639667e7032e06dd9ba
ryoya0427 <eihs9756@gmail.com>
2024-06-09 22:50:41 +0900

    문제 재 시도 페이지(미완)입니다.
    gpt iframe으로 사용하려 했으나 보안 이슈 때문에 iframe에서 사용할 수 있는 챗봇은 직접 만든 챗봇밖에 안된다고 해서 일단 안넣었습니다.
    이것도 동일하게 일단 무조건 성공 나오도록 처리해놨고 커밋 이후 바로 실제 코드로 작동하도록 수정시작하겠습니다.

--- src/views/SolutionPage.vue
+++ src/views/SolutionPage.vue
@@ -6,36 +6,66 @@
     >
       문제 목록
     </h2>
-    <ul v-if="showUserProblems" class="user-problems">
-      <li
-        v-for="problem in userProblems"
-        :key="problem.id"
-        @click="selectProblem(problem)"
-      >
-        {{ problem.title }}
-      </li>
-    </ul>
+    
 
     <div class="container">
       <div class="left-pane">
+        <ul v-if="showUserProblems" class="user-problems">
+          <li
+            v-for="problem in userProblems"
+            :key="problem.question_id"
+            @click="selectProblem(problem)"
+          >
+            {{ problem.question_summary }}
+          </li>
+        </ul>
+        <div v-if="problemSolution">
+          <button @click="openChatBot">챗봇 열기</button>
+        </div>
+      </div>
+
+      <div v-if="message" class="message-overlay">{{ message }}</div>
+
+      <div class="right-pane">
+        <select v-model="editorLanguage">
+            <option
+              v-for="language in languages"
+              :key="language.id"
+              :value="language.id"
+            >
+              {{ language.name }}
+            </option>
+          </select>
         <div v-if="selectedProblem">
-          <h3>선택한 문제: {{ selectedProblem.title }}</h3>
+          <h3>선택한 문제: {{ selectedProblem.question_summary }}</h3>
+          <p>{{ selectedProblem.question_detail }}</p>
+          <p v-if="selectedProblem.question_sample_input1">
+            예시 입력 1: {{ selectedProblem.question_sample_input1 }}
+          </p>
+          <p v-if="selectedProblem.question_sample_output1">
+            예시 출력 1: {{ selectedProblem.question_sample_output1 }}
+          </p>
+          <p v-if="selectedProblem.question_sample_input2">
+            예시 입력 2: {{ selectedProblem.question_sample_input2 }}
+          </p>
+          <p v-if="selectedProblem.question_sample_output2">
+            예시 출력 2: {{ selectedProblem.question_sample_output2 }}
+          </p>
+          <p v-if="selectedProblem.question_sample_input3">
+            예시 입력 3: {{ selectedProblem.question_sample_input3 }}
+          </p>
+          <p v-if="selectedProblem.question_sample_output3">
+            예시 출력 3: {{ selectedProblem.question_sample_output3 }}
+          </p>
           <textarea
             v-model="userCode"
             rows="10"
             cols="50"
-            placeholder="작성했던 코드 불러와줘야 할 듯"
+            placeholder="코드 작성"
           ></textarea>
           <button @click="submitCode">채점하기</button>
           <button @click="requestFeedback">문제 풀이 요청</button>
           <h3 v-if="gradingResult">{{ gradingResult }}</h3>
-        </div>
-      </div>
-
-      <div class="right-pane">
-        <div v-if="problemSolution">
-          <h3>문제 해답:</h3>
-          <pre>{{ problemSolution }}</pre>
         </div>
       </div>
     </div>
@@ -48,64 +78,150 @@ import axios from "axios";
 export default {
   data() {
     return {
-      userProblems: [
-        { id: 1, title: "문제1" },
-        { id: 2, title: "문제2" },
-        { id: 3, title: "문제3" },
-        { id: 4, title: "문제4" },
-        { id: 5, title: "문제5" }, // 임시로 넣음
-      ],
+      userProblems: [],
       selectedProblem: null,
       userCode: "",
-      feedback: "",
-      showUserProblems: false,
       gradingResult: "",
+      showUserProblems: false,
       problemSolution: "",
+      showChatGPT: true,
+      editorLanguage: "",
+      message: "",
+      languages: [
+        { id: 45, name: "Assembly (NASM 2.14.02)" },
+        { id: 46, name: "Bash (5.0.0)" },
+        { id: 47, name: "Basic (FBC 1.07.1)" },
+        { id: 75, name: "C (Clang 7.0.1)" },
+        { id: 76, name: "C++ (Clang 7.0.1)" },
+        { id: 48, name: "C (GCC 7.4.0)" },
+        { id: 52, name: "C++ (GCC 7.4.0)" },
+        { id: 49, name: "C (GCC 8.3.0)" },
+        { id: 53, name: "C++ (GCC 8.3.0)" },
+        { id: 50, name: "C (GCC 9.2.0)" },
+        { id: 54, name: "C++ (GCC 9.2.0)" },
+        { id: 86, name: "Clojure (1.10.1)" },
+        { id: 51, name: "C# (Mono 6.6.0.161)" },
+        { id: 77, name: "COBOL (GnuCOBOL 2.2)" },
+        { id: 55, name: "Common Lisp (SBCL 2.0.0)" },
+        { id: 90, name: "Dart (2.19.2)" },
+        { id: 56, name: "D (DMD 2.089.1)" },
+        { id: 57, name: "Elixir (1.9.4)" },
+        { id: 58, name: "Erlang (OTP 22.2)" },
+        { id: 44, name: "Executable" },
+        { id: 87, name: "F# (.NET Core SDK 3.1.202)" },
+        { id: 59, name: "Fortran (GFortran 9.2.0)" },
+        { id: 60, name: "Go (1.13.5)" },
+        { id: 95, name: "Go (1.18.5)" },
+        { id: 88, name: "Groovy (3.0.3)" },
+        { id: 61, name: "Haskell (GHC 8.8.1)" },
+        { id: 91, name: "Java (JDK 17.0.6)" },
+        { id: 62, name: "Java (OpenJDK 13.0.1)" },
+        { id: 63, name: "JavaScript (Node.js 12.14.0)" },
+        { id: 93, name: "JavaScript (Node.js 18.15.0)" },
+        { id: 78, name: "Kotlin (1.3.70)" },
+        { id: 64, name: "Lua (5.3.5)" },
+        { id: 89, name: "Multi-file program" },
+        { id: 79, name: "Objective-C (Clang 7.0.1)" },
+        { id: 65, name: "OCaml (4.09.0)" },
+        { id: 66, name: "Octave (5.1.0)" },
+        { id: 67, name: "Pascal (FPC 3.0.4)" },
+        { id: 85, name: "Perl (5.28.1)" },
+        { id: 68, name: "PHP (7.4.1)" },
+        { id: 43, name: "Plain Text" },
+        { id: 69, name: "Prolog (GNU Prolog 1.4.5)" },
+        { id: 70, name: "Python (2.7.17)" },
+        { id: 92, name: "Python (3.11.2)" },
+        { id: 71, name: "Python (3.8.1)" },
+        { id: 80, name: "R (4.0.0)" },
+        { id: 72, name: "Ruby (2.7.0)" },
+        { id: 73, name: "Rust (1.40.0)" },
+        { id: 81, name: "Scala (2.13.2)" },
+        { id: 82, name: "SQL (SQLite 3.27.2)" },
+        { id: 83, name: "Swift (5.2.3)" },
+        { id: 74, name: "TypeScript (3.7.4)" },
+        { id: 94, name: "TypeScript (5.0.3)" },
+        { id: 84, name: "Visual Basic.Net (vbnc 0.0.0.5943)" },
+      ],
     };
   },
   mounted() {
-    axios
-      .get("/user/problems")
-      .then((response) => {
-        this.userProblems = response.data;
-      })
-      .catch((error) => {
-        console.error("문제 목록을 불러오는 중 에러 발생: ", error);
-      });
+    this.fetchUserProblems();
   },
   methods: {
     selectProblem(problem) {
       this.selectedProblem = problem;
       this.gradingResult = "";
-      this.problemSolution = "";
     },
-    submitCode() {
-      axios
-        .post("/grade", {
-          problemId: this.selectedProblem.id,
-          code: this.userCode,
-        })
-        .then((response) => {
-          console.log("채점 결과:", response.data);
-          this.gradingResult = `채점 결과: '${response.data}'`;
-        })
-        .catch((error) => {
-          console.error("채점 요청 중 에러 발생: ", error);
-        });
+    async fetchUserProblems() {
+      const token = localStorage.getItem("authToken"); // 로컬 스토리지에서 토큰을 가져옴
+      try {
+        const response = await axios.get(
+          "https://destiny-back-63f6h32ypq-de.a.run.app/blue/question/get_my_question",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        this.userProblems = response.data;
+        this.showMessage("이전 문제를 성공적으로 불러왔습니다.");
+      } catch (error) {
+        console.error("에러 발생:", error);
+        console.log("에러 응답 데이터:", error.response?.data);
+        this.showMessage(
+          `이전 문제 불러오기 중 오류가 발생했습니다: ${
+            error.response?.data?.message || error.message
+          }`
+        );
+      }
     },
-    requestFeedback() {
-      axios
-        .post("/request-feedback", {
-          problemId: this.selectedProblem.id,
-          code: this.userCode,
-        })
-        .then((response) => {
-          console.log("챗 지피티 피드백:", response.data);
-          this.problemSolution = response.data.solution;
-        })
-        .catch((error) => {
-          console.error("문제 풀이 요청 중 에러 발생: ", error);
-        });
+    async submitCode() {
+      const token = localStorage.getItem("authToken");
+      /*
+      const data = {
+        user_code: this.userCode,
+        question_id: this.selectedProblem.question_id,
+        language_id: this.editorLanguage,
+      };
+      */
+      try {
+        console.log("API 요청 시작");
+        const response = await axios.post(
+          "https://destiny-back-63f6h32ypq-de.a.run.app/blue/question/check_answer",
+          {
+            "user_code" : "print(\"a\")",
+            "question_id" : 4,
+            "language_id" : "71",
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        this.result = response.data; // 결과를 result 변수에 저장
+        if (this.result === "Success : True") {
+          this.showMessage("정답입니다 ");
+          this.gradingResult = "정답입니다.";
+        } else {
+          this.showMessage("오답입니다 ");
+          this.gradingResult = "오답입니다.";
+        }
+      } catch (error) {
+        console.error("채점 요청 중 에러 발생: ", error);
+      }
+    },
+    chatButton() {
+      this.showChatGPT = !this.showChatGPT;
+    },  
+    openChatBot() {
+      window.location.href = "https://chatgpt.com";
+    },
+    showMessage(message) {
+      this.message = message;
+      setTimeout(() => {
+        this.message = "";
+      }, 2000); // 메시지가 2초 후에 사라지도록 설정
     },
   },
 };
@@ -160,5 +276,18 @@ button {
 pre {
   white-space: pre-wrap;
   word-wrap: break-word;
+}
+.message-overlay {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: rgba(0, 58, 154, 0.9);
+  color: #fff;
+  padding: 20px;
+  border-radius: 4px;
+  text-align: center;
+  z-index: 1000;
+  font-size: 1.5em;
 }
 </style>
